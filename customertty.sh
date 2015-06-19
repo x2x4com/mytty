b1b71c0e9b4465d854a44c3d372e2e0c24d39cd1
许向 <x2x4@x2x4.net>
2015-06-20 02:04:44 +0800

    update

--- customertty.sh
+++ customertty.sh
@@ -5,15 +5,18 @@
 #
 #        USAGE:  . customertty.sh 
 #
-#  DESCRIPTION:  Add Darwin support, tested on Centos/RHEL5, ubuntu 12.04LTS
+#  DESCRIPTION:  Add Darwin support, tested on Centos/RHEL5/6, ubuntu 12.04LTS
+#               
+#       UPDATE:  20140621 增加了类似eth0:0 这样的方式显示
+#                20150620 增加了自适应的横线，仅限于第一次适应
 #
 #      OPTIONS:  ---
 # REQUIREMENTS:  ---
 #         BUGS:  ---
 #        NOTES:  ---
 #       AUTHOR:  xuxiang@zrit100.com ; x2x4com@gmail.com
-#      COMPANY:  zrit
-#      VERSION:  2.1 
+#      COMPANY:  ZRIT
+#      VERSION:  2.3
 #      CREATED:  11/12/2012 10:50:00 AM
 #     REVISION:  001
 #===============================================================================
@@ -23,6 +26,13 @@
 
 OLDLANG=$LANG
 export LANG=C
+COLUMNS=$(tput cols)
+if [ -n $COLUMNS ]
+  then
+  width=$(perl -e "print '-' x $COLUMNS")
+else
+  width="---------------------------------------------------------------------------------------------"
+fi
 
 IPADDRS=`perl <<'END'
 use strict;
@@ -32,7 +42,7 @@ use Data::Dumper;
 
 my @ifconfig_source = \`/sbin/ifconfig\`;
 my $if_hash = {}; 
-my $show_int = "^(en|br|tun|eth|em|bond|tap)";
+my $show_int = "^(en|br|tun|eth|em)";
 my $int_name;
 
 foreach my $line (@ifconfig_source) {
@@ -74,10 +84,9 @@ END
 `
 if [ "`id -u`" -eq 0 ]
 then
-	#PS1="---------------------------------------------------------------------------------------------\n\n[\033[34m\u\e[m@\033[32m\H\e[m] \033[1;33m$IPADDRS\e[m \n[\t] PWD => \033[1;35m\w\e[m\n\#># "
-	PS1="---------------------------------------------------------------------------------------------\n\n[\033[32m\u\e[m@\033[31m\H\e[m] \033[33m$IPADDRS\e[m \n[\t] PWD => \033[1;35m\w\e[m\n\#># "
+	PS1="${width}\n\n[\033[32m\u\e[m@\033[31m\H\e[m] \033[33m$IPADDRS\e[m \n[\t] PWD => \033[1;35m\w\e[m\n\#># "
 else
-	PS1="---------------------------------------------------------------------------------------------\n\n[\033[32m\u\e[m@\033[31m\H\e[m] \033[33m$IPADDRS\e[m \n[\t] PWD => \033[1;35m\w\e[m\n\#>$ "
+	PS1="${width}\n\n[\033[32m\u\e[m@\033[31m\H\e[m] \033[33m$IPADDRS\e[m \n[\t] PWD => \033[1;35m\w\e[m\n\#>$ "
 fi
 
 export LANG=$OLDLANG
